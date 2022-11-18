695203fa26c3fb40af7401613573a688a90602f8
nikhisri <92200221+nikhisri@users.noreply.github.com>
2022-11-18 19:44:20 +0530

    Delete maill.py

--- Project Development Phase/Sprint 4/Final_project/maill.py
+++
@@ -1,6 +0,0 @@
-import smtplib 
-import ssl
-server = smtplib.SMTP_SSL('smtp.mail.yahoo.com',port=465)
-server.login("nikhisri@yahoo.com","Nk100262")
-server.sendmail("nikhisri@yahoo.com", "nikhisrinivas261@gmail.com", "reorder")
-server.quit()
